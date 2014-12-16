b951bec682b8e10f29394dec1bd246a65ce47a34
psychowood <psychowood@gmail.com>
2014-12-16 18:01:53 +0100

    Skeleton for test
    Still not used and excluded from the build

---
+++ test/spec/services/utorrentservice.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: uTorrentService', function () {
+
+  // load the service's module
+  beforeEach(module('utorrentNgwebuiApp'));
+
+  // instantiate service
+  var uTorrentService;
+  beforeEach(inject(function (_uTorrentService_) {
+    uTorrentService = _uTorrentService_;
+  }));
+
+  it('should do something', function () {
+    expect(!!uTorrentService).toBe(true);
+  });
+
+});
