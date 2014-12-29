e479eb85cb138780a572610d690dc4a133ddd67f
psychowood <psychowood@gmail.com>
2014-12-29 14:51:26 +0100

    Removed unused/commented code

--- app/scripts/directives/torrent-row.js
+++ app/scripts/directives/torrent-row.js
@@ -14,86 +14,81 @@ angular.module('utorrentNgwebuiApp')
     restrict: 'E',
     replace: true,
     link: function postLink(scope /*, element, attrs */) {
+
       var item = scope.item;
-      if (item) {
-        //Order of execution
+      //Preserve order of execution
 
-        //var statuses = item.getStatuses();
+      var statusClass,statusTitle,statusColor;
+      var btnClass,btnIcon,btnAction;
 
-        var statusClass,statusTitle,statusColor;
-        var btnClass,btnIcon,btnAction;
+      item.getStatuses();
 
-        item.getStatuses();
-
-        if(item.isStatusError() && !item.isStatusCompleted()) {
-          statusClass = 'exclamation-sign';
-          statusTitle = 'Error';
-          statusColor = 'text-danger';
-        } else if(!item.isStatusLoaded()) {
-          statusClass = 'warning-sign';
-          statusTitle = 'Torrent not loaded';
-          statusColor = 'text-danger';
-        } else if(item.isStatusChecking()) {
-          statusClass = 'repeat';
-          statusTitle = 'Checking';
-          statusColor = 'text-info';
-        } else if(!item.isStatusChecked()) {
-          statusClass = 'warning-sign';
-          statusTitle = 'Torrent needs checking';
-          statusColor = 'text-warning';
-        } else if(item.isStatusPaused()) {
-          statusClass = 'pause';
-          statusTitle = 'Paused';
-          statusColor = 'text-info';
-        } else if(item.isStatusSeeding()) {
-          statusClass = 'collapse-up';
-          statusTitle = 'Seeding';
+      if(item.isStatusError() && !item.isStatusCompleted()) {
+        statusClass = 'exclamation-sign';
+        statusTitle = 'Error';
+        statusColor = 'text-danger';
+      } else if(!item.isStatusLoaded()) {
+        statusClass = 'warning-sign';
+        statusTitle = 'Torrent not loaded';
+        statusColor = 'text-danger';
+      } else if(item.isStatusChecking()) {
+        statusClass = 'repeat';
+        statusTitle = 'Checking';
+        statusColor = 'text-info';
+      } else if(!item.isStatusChecked()) {
+        statusClass = 'warning-sign';
+        statusTitle = 'Torrent needs checking';
+        statusColor = 'text-warning';
+      } else if(item.isStatusPaused()) {
+        statusClass = 'pause';
+        statusTitle = 'Paused';
+        statusColor = 'text-info';
+      } else if(item.isStatusSeeding()) {
+        statusClass = 'collapse-up';
+        statusTitle = 'Seeding';
+        statusColor = 'text-success';
+      } else if(item.isStatusDownloading()) {
+        statusClass = 'collapse-down';
+        statusTitle = 'Downloading';
+        statusColor = 'text-info';
+        btnClass = 'warning';
+        btnIcon = 'pause';
+        btnAction = 'pause';
+      } else if(item.isStatusStartAfterCheck()) {
+        statusClass = 'repeat';
+        statusTitle = 'Start after checking';
+        statusColor = 'text-info';
+      } else if(item.isStatusQueued()) {
+        statusClass = 'time';
+        statusTitle = 'Queued';
+        statusColor = 'text-info';
+      } else if(item.isStatusLoaded() && item.isStatusChecked()) {
+        if (item.isStatusCompleted()) {
+          statusClass = 'check';
+          statusTitle = 'Completed';
           statusColor = 'text-success';
-        } else if(item.isStatusDownloading()) {
-          statusClass = 'collapse-down';
-          statusTitle = 'Downloading';
-          statusColor = 'text-info';
-          btnClass = 'warning';
-          btnIcon = 'pause';
-          btnAction = 'pause';
-        } else if(item.isStatusStartAfterCheck()) {
-          statusClass = 'repeat';
-          statusTitle = 'Start after checking';
-          statusColor = 'text-info';
-        } else if(item.isStatusQueued()) {
-          statusClass = 'time';
-          statusTitle = 'Queued';
-          statusColor = 'text-info';
-        } else if(item.isStatusLoaded() && item.isStatusChecked()) {
-          if (item.isStatusCompleted()) {
-            statusClass = 'check';
-            statusTitle = 'Completed';
-            statusColor = 'text-success';
-            btnClass = '';
-            btnIcon = 'stop';
-            btnAction = 'stop';
-          } else {
-            statusClass = 'unchecked';
-            statusTitle = 'Ready';
-            statusColor = '';
-            btnClass = 'success';
-            btnIcon = 'play-circle';
-            btnAction = 'start';
-          }
+          btnClass = '';
+          btnIcon = 'stop';
+          btnAction = 'stop';
         } else {
-          statusClass = 'question-sign';
-          statusTitle = 'Status not supported: ' + parseInt(item.status).toString(2);
+          statusClass = 'unchecked';
+          statusTitle = 'Ready';
+          statusColor = '';
+          btnClass = 'success';
+          btnIcon = 'play-circle';
+          btnAction = 'start';
         }
-
-        scope.statusClass = 'glyphicon-' + statusClass;
-        scope.statusTitle = statusTitle;
-        scope.statusColor = statusColor;
-        scope.btnClass = 'btn-' + btnClass;
-        scope.btnIcon = 'glyphicon-' + btnIcon;
-
       } else {
-        console.log('undefined');
+        statusClass = 'question-sign';
+        statusTitle = 'Status not supported: ' + parseInt(item.status).toString(2);
       }
+
+      scope.statusClass = 'glyphicon-' + statusClass;
+      scope.statusTitle = statusTitle;
+      scope.statusColor = statusColor;
+      scope.btnClass = 'btn-' + btnClass;
+      scope.btnIcon = 'glyphicon-' + btnIcon;
+
     }
   };
 
