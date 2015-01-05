58e427da4b5b2e37538e5344e744a8cc87ee16b3
psychowood <psychowood@gmail.com>
2015-01-05 19:11:28 +0100

    Issue reporting: more infos, and also via email

--- app/scripts/controllers/issue-reporter.js
+++ app/scripts/controllers/issue-reporter.js
@@ -9,15 +9,34 @@
  */
 angular.module('utorrentNgwebuiApp')
   .controller('IssueReporterCtrl', function ($scope,uTorrentService,$window) {
-    var version = 'undefined';
     var newIssueBaseUrl = 'https://github.com/psychowood/ng-torrent-ui/issues/new/';
+    var newMailBaseUrl = 'mailto:ng' + 'torrent' + 'ui' + '@' + 'gmail.com';
     $scope.newIssueUrl = newIssueBaseUrl;
     // uTorrentService.init().then(function(){
     // });
 
-    $scope.prepareReport = function() {
-      version = uTorrentService.getVersion();
-      $scope.newIssueUrl = newIssueBaseUrl + '?body=%0A%0A' + $window.encodeURIComponent('**'+version + '**');
-      return true;
+    var getBody = function() {
+      var version = uTorrentService.getVersion();
+      return  'Describe your issue here' +
+        '%0A' +
+        '%0A' +
+        '------------------------' +
+        '%0A' +
+        $window.encodeURIComponent('**Version:** '+$window.document.title) +
+        '%0A' +
+        $window.encodeURIComponent('**Server:** '+version) +
+        '%0A' +
+        $window.encodeURIComponent('**User-agent:** '+$window.navigator.userAgent);
     };
+
+    $scope.prepareNewIssueUrl = function() {
+      $scope.newIssueUrl = newIssueBaseUrl + '?body=' + getBody();
+      return false;
+    };
+
+    $scope.prepareNewMailUrl = function() {
+      $scope.newMailUrl = newMailBaseUrl + '?body=' + getBody();
+      return false;
+    };
+
   });
